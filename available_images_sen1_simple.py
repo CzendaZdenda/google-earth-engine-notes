dbba0e63848982c4a66b3ee3e3def206d1694686
CzendaZdenda <tramtara@seznam.cz>
2020-11-19 15:49:07 +0700

    Add script to get available Sentinel-1 images over some area.
    Also add some description into README.

---
+++ available_images_sen1_simple.py
@@ -0,0 +1,51 @@
+# -*- coding: utf-8 -*-
+#
+#   Get some information about images, that are available over your area.
+#
+#   In this example the "COPERNICUS/S1_GRD" dataset (Sentinel-1 SAR GRD: C-band Synthetic Aperture Radar Ground
+# Range Detected, log scaling) is used [1].
+#
+# [1] https://developers.google.com/earth-engine/datasets/catalog/COPERNICUS_S1_GRD
+
+import ee
+
+
+def main():
+    # uncomment in case you did not authenticate to Earth Engine yet
+    # ee.Authenticate()
+
+    # initialize ee library
+    ee.Initialize()
+
+    # define an area of interest (eg. Chara Sands)
+    region = ee.Geometry.Polygon(
+        [[118.06526184082031, 56.81102820967043],
+         [118.20945739746094, 56.81102820967043],
+         [118.20945739746094, 56.881062658158335],
+         [118.06526184082031, 56.881062658158335],
+         [118.06526184082031, 56.81102820967043]])
+
+    # set dataset
+    dataset_name = "COPERNICUS/S1_GRD"
+
+    sen_collection = ee.ImageCollection(dataset_name) \
+        .filterBounds(region)
+
+    collection_info = sen_collection.getInfo()
+
+    for k in collection_info["features"]:
+        properties = k['properties']
+
+        ori_filename = properties['system:index']
+        res_m = properties['resolution_meters']
+        pol = properties['transmitterReceiverPolarisation']
+        plat = properties['platform_number']
+        plat_name = properties['familyName']
+        orbit = properties['orbitProperties_pass']
+        mode = properties['instrumentMode']
+
+        print(f'{ori_filename}, {res_m}m, {pol}, {plat_name}{plat}, {orbit}, {mode}')
+
+
+if __name__ == '__main__':
+    main()
